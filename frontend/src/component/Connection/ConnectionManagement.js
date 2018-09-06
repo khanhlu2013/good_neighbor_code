bd6e087082f1931a235e714fdfa0a4d9e00d5cc7
Khanh Tran <lu@mac.local>
2018-09-06 15:44:45 -0700

    refactor connectionManagement component to remove connectionTables

--- frontend/src/component/Connection/ConnectionManagement.js
+++ frontend/src/component/Connection/ConnectionManagement.js
@@ -2,23 +2,85 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { SearchByEmail } from "./SearchByEmail.js";
-import { ConnectionTables } from "./ConnectionTables.js";
+import { ConnectionTable } from "./ConnectionTable.js";
 
 function ConnectionManagement(props) {
+  const {
+    loginUser: { id: loginUserId },
+    connections,
+    updateConnectionCb
+  } = props;
+
+  const friends = connections.filter(
+    connection => connection.approvedByFrom && connection.approvedByTo
+  );
+  const inFriends = connections.filter(
+    connection =>
+      connection.to.id === loginUserId &&
+      connection.approvedByFrom &&
+      connection.approvedByTo === undefined
+  );
+  const outFriends = connections.filter(
+    connection =>
+      connection.from.id === loginUserId &&
+      connection.approvedByFrom &&
+      connection.approvedByTo === undefined
+  );
+  const rejectedFriends = connections.filter(
+    connection =>
+      connection.to.id === loginUserId && connection.approvedByTo === false
+  );
+
   return (
-    <div id="ConnectionManagement-react">
+    <div id="ConnectionManagement-react" className="container">
       <SearchByEmail
         loginUser={props.loginUser}
         connections={props.connections}
         createConnectionCb={props.createConnectionCb}
         updateConnectionCb={props.updateConnectionCb}
       />
-      <ConnectionTables
-        loginUserId={props.loginUser.id}
-        connections={props.connections}
-        updateConnectionCb={props.updateConnectionCb}
-      />
-      <button>Add Image</button>
+      <div className="row">
+        <div className="col-sm">
+          <ConnectionTable
+            id="FriendTable"
+            title="Friends"
+            connections={friends}
+            loginUserId={loginUserId}
+            approveColumn={undefined}
+            denyColumn="remove"
+            updateConnectionCb={updateConnectionCb}
+          />
+        </div>
+        <div className="col-sm">
+          <ConnectionTable
+            id="InFriendTable"
+            title="Friend Incomming Requests"
+            connections={inFriends}
+            loginUserId={loginUserId}
+            approveColumn="approve"
+            denyColumn="deny"
+            updateConnectionCb={updateConnectionCb}
+          />
+          <ConnectionTable
+            id="OutFriendTable"
+            title="Your Outgoing Requests"
+            connections={outFriends}
+            loginUserId={loginUserId}
+            approveColumn={undefined}
+            denyColumn="undo"
+            updateConnectionCb={updateConnectionCb}
+          />
+          <ConnectionTable
+            id="DenyFriendTable"
+            title="Your denal of Friend Incomming Requests"
+            connections={rejectedFriends}
+            loginUserId={loginUserId}
+            approveColumn="undo"
+            denyColumn={undefined}
+            updateConnectionCb={updateConnectionCb}
+          />
+        </div>
+      </div>
       {props.isRefreshingConnections && <p>Refreshing Connection ...</p>}
     </div>
   );
--- frontend/src/component/Connection/ConnectionTable.js
+++ frontend/src/component/Connection/ConnectionTable.js
@@ -1,82 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function ConnectionTables(props) {
-  const { loginUserId, connections, updateConnectionCb } = props;
-
-  const friends = connections.filter(
-    connection => connection.approvedByFrom && connection.approvedByTo
-  );
-  const inFriends = connections.filter(
-    connection =>
-      connection.to.id === loginUserId &&
-      connection.approvedByFrom &&
-      connection.approvedByTo === undefined
-  );
-  const outFriends = connections.filter(
-    connection =>
-      connection.from.id === loginUserId &&
-      connection.approvedByFrom &&
-      connection.approvedByTo === undefined
-  );
-  const rejectedFriends = connections.filter(
-    connection =>
-      connection.to.id === loginUserId && connection.approvedByTo === false
-  );
-
-  return (
-    <div id="ConnectionTables-react" className="container">
-      <div className="row">
-        <div className="col-sm">
-          <ConnectionTable
-            id="FriendTable"
-            title="Friends"
-            connections={friends}
-            loginUserId={loginUserId}
-            approveColumn={undefined}
-            denyColumn="remove"
-            updateConnectionCb={updateConnectionCb}
-          />
-        </div>
-        <div className="col-sm">
-          <ConnectionTable
-            id="InFriendTable"
-            title="Friend Incomming Requests"
-            connections={inFriends}
-            loginUserId={loginUserId}
-            approveColumn="approve"
-            denyColumn="deny"
-            updateConnectionCb={updateConnectionCb}
-          />
-          <ConnectionTable
-            id="OutFriendTable"
-            title="Your Outgoing Requests"
-            connections={outFriends}
-            loginUserId={loginUserId}
-            approveColumn={undefined}
-            denyColumn="undo"
-            updateConnectionCb={updateConnectionCb}
-          />
-          <ConnectionTable
-            id="DenyFriendTable"
-            title="Your denal of Friend Incomming Requests"
-            connections={rejectedFriends}
-            loginUserId={loginUserId}
-            approveColumn="undo"
-            denyColumn={undefined}
-            updateConnectionCb={updateConnectionCb}
-          />
-        </div>
-      </div>
-    </div>
-  );
-}
-ConnectionTables.propTypes = {
-  loginUserId: PropTypes.string.isRequired,
-  connections: PropTypes.array.isRequired,
-  updateConnectionCb: PropTypes.func.isRequired
-};
-
 function ConnectionTable(props) {
   const {
     id,
@@ -181,4 +105,4 @@ ConnectionRow.propTypes = {
   updateConnectionCb: PropTypes.func.isRequired
 };
 
-export { ConnectionTables };
+export { ConnectionTable };
