0d8a8b474d5581dbcf15bbc89b53a75d5ad8242d
Khanh Tran <lu@mac.local>
2018-09-10 10:46:03 -0700

    privatized impost

--- frontend/src/AppPrivate.js
+++ frontend/src/AppPrivate.js
@@ -14,17 +14,12 @@ class PrivateApp extends Component {
   state = {
     connections: [],
     isRefreshingConnections: false,
-    inPosts: [],
-    isRefreshingInPosts: false,
     requestingOutPostCount: null
   };
 
   componentDidMount() {
     this.setState({ isRefreshingConnections: true });
     this.refreshConnections();
-
-    this.setState({ isRefreshingInPosts: true });
-    this.refreshInPosts();
 
     (async () => {
       const outPosts = await API.outPosts();
@@ -37,17 +32,6 @@ class PrivateApp extends Component {
   refreshConnections = async () => {
     const connections = await API.connections();
     this.setState({ connections, isRefreshingConnections: false });
-  };
-
-  refreshInPosts = async () => {
-    const inPosts = await API.inPosts();
-    const inPostsFilterDeny = inPosts.filter(inPost =>
-      inPost.denied.every(
-        share => share.borrower.id !== this.props.loginUser.id
-      )
-    );
-
-    this.setState({ inPosts: inPostsFilterDeny, isRefreshingInPosts: false });
   };
 
   createConnectionCb = userIdToAdd => {
@@ -64,38 +48,6 @@ class PrivateApp extends Component {
       await API.updateConnection(connectionId, isApproved);
       this.refreshConnections();
     })();
-  };
-
-  onCreateRequestingShareCb = postID => {
-    this.setState({ isRefreshingInPosts: true });
-    (async () => {
-      await API.createShare(postID);
-      this.refreshInPosts();
-    })();
-  };
-
-  onDeleteRequestingShareCb = shareID => {
-    this.setState({ isRefreshingInPosts: true });
-    (async () => {
-      await API.deleteShare(shareID);
-      this.refreshInPosts();
-    })();
-  };
-
-  onReturnBorrowingShareCb = shareID => {
-    const [post] = this.state.inPosts.filter(
-      post => post.shares.filter(share => share.id === shareID).length === 1
-    );
-    if (
-      window.confirm(`You are returning ${post.title}. This can not be undo!`)
-    ) {
-      this.setState({ isRefreshingInPosts: true });
-      (async () => {
-        const isReturnedByTo = true;
-        await API.updateInShare(shareID, isReturnedByTo);
-        this.refreshInPosts();
-      })();
-    }
   };
 
   doUpdateRequestingOutPostCount = count => {
@@ -138,14 +90,7 @@ class PrivateApp extends Component {
             </Tab>
           </TabList>
           <TabPanel>
-            <InPostManagement
-              loginUser={this.props.loginUser}
-              inPosts={this.state.inPosts}
-              isRefreshingInPosts={this.state.isRefreshingInPosts}
-              onDeleteRequestingShareCb={this.onDeleteRequestingShareCb}
-              onCreateRequestingShareCb={this.onCreateRequestingShareCb}
-              onReturnBorrowingShareCb={this.onReturnBorrowingShareCb}
-            />
+            <InPostManagement loginUser={this.props.loginUser} />
           </TabPanel>
           <TabPanel>
             <OutPostManagement
--- frontend/src/component/InPost/InPostManagement.js
+++ frontend/src/component/InPost/InPostManagement.js
@@ -1,55 +1,120 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
+import className from "classnames";
+
 import { InPostTable } from "./InPostTable";
 import { InShareRequestingTable } from "./InShareRequestingTable";
 import { InShareBorrowingTable } from "./InShareBorrowingTable";
 import { InShareBorrowedTable } from "./InShareBorrowedTable";
+import { API } from "../../api/profile-api";
 
-function InPostManagement(props) {
-  const {
-    loginUser,
-    inPosts,
-    isRefreshingInPosts,
-    onCreateRequestingShareCb,
-    onDeleteRequestingShareCb,
-    onReturnBorrowingShareCb
-  } = props;
-  const myInShares2D = inPosts.map(post =>
-    post.shares.filter(share => share.borrower.id === loginUser.id)
-  );
-  const myInShares1D = [].concat(...myInShares2D);
-  const requestingShares = myInShares1D.filter(share => share.isRequesting);
-  const borrowingShares = myInShares1D.filter(share => share.isBorrowing);
-  const borrowedShares = myInShares1D.filter(share => share.isBorrowed);
+class InPostManagement extends Component {
+  state = {
+    inPosts: null
+  };
 
-  return (
-    <div id="InPostManagement-react">
-      <h1>InPosts Managements</h1>
-      <InPostTable
-        loginUser={loginUser}
-        allInPosts={inPosts}
-        onCreateRequestingShareCb={onCreateRequestingShareCb}
-      />
-      <InShareRequestingTable
-        shares={requestingShares}
-        onDeleteRequestingShareCb={onDeleteRequestingShareCb}
-      />
-      <InShareBorrowingTable
-        shares={borrowingShares}
-        onReturnBorrowingShareCb={onReturnBorrowingShareCb}
-      />
-      <InShareBorrowedTable shares={borrowedShares} />
-      {isRefreshingInPosts && <p>Refreshing in posts ...</p>}
-    </div>
-  );
+  static getDerivedStateFromProps(props, state) {
+    let requestingShares = null;
+    let borrowingShares = null;
+    let borrowedShares = null;
+
+    if (state.inPosts) {
+      const myInShares2D = state.inPosts.map(post =>
+        post.shares.filter(share => share.borrower.id === props.loginUser.id)
+      );
+      const myInShares1D = [].concat(...myInShares2D);
+      requestingShares = myInShares1D.filter(share => share.isRequesting);
+      borrowingShares = myInShares1D.filter(share => share.isBorrowing);
+      borrowedShares = myInShares1D.filter(share => share.isBorrowed);
+    }
+
+    return {
+      requestingShares,
+      borrowingShares,
+      borrowedShares
+    };
+  }
+
+  componentDidMount() {
+    this.refreshInPosts();
+  }
+
+  refreshInPosts = async () => {
+    const inPosts = await API.inPosts();
+    const inPostsFilterDeny = inPosts.filter(inPost =>
+      inPost.denied.every(
+        share => share.borrower.id !== this.props.loginUser.id
+      )
+    );
+
+    this.setState({ inPosts: inPostsFilterDeny });
+  };
+
+  doCreateRequestingShare = postID => {
+    this.setState({ inPosts: null });
+    (async () => {
+      await API.createShare(postID);
+      this.refreshInPosts();
+    })();
+  };
+
+  doDeleteRequestingShare = shareID => {
+    this.setState({ inPosts: null });
+    (async () => {
+      await API.deleteShare(shareID);
+      this.refreshInPosts();
+    })();
+  };
+
+  doReturnBorrowingShare = shareID => {
+    const [post] = this.state.inPosts.filter(
+      post => post.shares.filter(share => share.id === shareID).length === 1
+    );
+    if (
+      window.confirm(`You are returning ${post.title}. This can not be undo!`)
+    ) {
+      this.setState({ inPosts: null });
+      (async () => {
+        const isReturnedByTo = true;
+        await API.updateInShare(shareID, isReturnedByTo);
+        this.refreshInPosts();
+      })();
+    }
+  };
+
+  render() {
+    const isRefreshingInPosts = this.state.inPosts !== null;
+    return (
+      <div
+        id="InPostManagement-react"
+        className={className({ isRefreshingInPosts })}
+      >
+        <h1>InPosts Managements</h1>
+
+        {isRefreshingInPosts && (
+          <div>
+            <InPostTable
+              loginUser={this.props.loginUser}
+              allInPosts={this.state.inPosts}
+              onCreateRequestingShareCb={this.doCreateRequestingShare}
+            />
+            <InShareRequestingTable
+              shares={this.state.requestingShares}
+              onDeleteRequestingShareCb={this.doDeleteRequestingShare}
+            />
+            <InShareBorrowingTable
+              shares={this.state.borrowingShares}
+              onReturnBorrowingShareCb={this.doReturnBorrowingShare}
+            />
+            <InShareBorrowedTable shares={this.state.borrowedShares} />
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 InPostManagement.propTypes = {
-  loginUser: PropTypes.object.isRequired,
-  inPosts: PropTypes.array.isRequired,
-  isRefreshingInPosts: PropTypes.bool.isRequired,
-  onDeleteRequestingShareCb: PropTypes.func.isRequired,
-  onCreateRequestingShareCb: PropTypes.func.isRequired,
-  onReturnBorrowingShareCb: PropTypes.func.isRequired
+  loginUser: PropTypes.object.isRequired
 };
 
 export { InPostManagement };
