e948dbda402c10fa444ffa277ad01329abffb44c
Khanh Tran <lu@mac.local>
2018-09-25 15:38:15 -0700

    break up app notification test into static and dynamic files

---
+++ frontend/cypress/integration/notification/static.js
@@ -0,0 +1,125 @@
+import { ObjectID } from "mongodb";
+import { tab } from "../../helper/ui.js";
+import {
+  createUser,
+  createConnection,
+  createPost,
+  createShare
+} from "../../helper/model.js";
+
+describe("notification static display ui", () => {
+  const lu = createUser("Lu Tran", "lu@us.com");
+  const tu = createUser("Tu Nguyen", "tu@pr.com");
+
+  describe("connection", () => {
+    it("can show friend request", () => {
+      //friend request
+      const connection = createConnection(tu, lu, true, undefined);
+
+      cy.setupDb([lu, tu], [connection]);
+      cy.loadApp();
+      cy.login(lu.email);
+
+      tab.connection.snapRightAway();
+    });
+  });
+
+  describe("my post", () => {
+    it("can show request", () => {
+      const connection = createConnection(tu, lu, true, true);
+      const post = createPost(lu, "title", "description");
+      const share = createShare(post, tu);
+      cy.setupDb([lu, tu], [connection], [post], [share]);
+      cy.loadApp();
+      cy.login(lu.email);
+      tab.outPost.focus();
+      tab.outPost.snap("master");
+      tab.outPost.waitingList.snapRightAway("request");
+    });
+    it("can show unaware return", () => {
+      const connection = createConnection(tu, lu, true, true);
+      const post = createPost(lu, "title", "description");
+      const isApprove = true;
+      const isAwareApprove = true;
+      const isReturn = true;
+      const isAwareReturn = false;
+      const dateReturn = new Date();
+      const share = createShare(
+        post,
+        tu,
+        isApprove,
+        isAwareApprove,
+        isReturn,
+        isAwareReturn,
+        dateReturn
+      );
+      cy.setupDb([lu, tu], [connection], [post], [share]);
+      cy.loadApp();
+      cy.login(lu.email);
+      tab.outPost.focus();
+      tab.outPost.snap("master");
+      tab.outPost.return.snapRightAway("return");
+    });
+    it("can show combine request and unaware return", () => {
+      const connection = createConnection(tu, lu, true, true);
+      const requestPost = createPost(lu, "title", "description");
+      const requestShare = createShare(requestPost, tu);
+      const returnPost = createPost(lu, "title", "description");
+      const isApprove = true;
+      const isAwareApprove = true;
+      const isReturn = true;
+      const isAwareReturn = false;
+      const dateReturn = new Date();
+      const returnShare = createShare(
+        returnPost,
+        tu,
+        isApprove,
+        isAwareApprove,
+        isReturn,
+        isAwareReturn,
+        dateReturn
+      );
+      cy.setupDb(
+        [lu, tu],
+        [connection],
+        [requestPost, returnPost],
+        [requestShare, returnShare]
+      );
+      cy.loadApp();
+      cy.login(lu.email);
+      tab.outPost.focus();
+      tab.outPost.snapRightAway("master");
+      tab.outPost.waitingList.snapRightAway("request");
+      tab.outPost.return.snapRightAway("return");
+    });
+  });
+
+  describe("friend post", () => {
+    it("can show approve post", () => {
+      const connection = createConnection(tu, lu, true, true);
+      const post = createPost(tu, "title", "description");
+      const isApprove = true;
+      const isAwareApprove = false;
+      const isReturn = false;
+      const isAwareReturn = false;
+      const dateReturn = null;
+      const share = createShare(
+        post,
+        lu,
+        isApprove,
+        isAwareApprove,
+        isReturn,
+        isAwareReturn,
+        dateReturn
+      );
+
+      cy.setupDb([lu, tu], [connection], [post], [share]);
+      cy.loadApp();
+      cy.login(lu.email);
+
+      tab.inPost.focus();
+      tab.inPost.snap("master");
+      tab.inPost.approve.snapRightAway("approve");
+    });
+  });
+});
--- frontend/snapshots.js
+++ frontend/snapshots.js
@@ -1,32 +1,30 @@
 module.exports = {
   "__version": "3.1.0",
-  "notification": {
-    "static data display": {
-      "connection": {
-        "can show friend request": {
-          "1": "<span id=\"tabSelector_connection\">friends<span class=\"text-danger\"> (1)</span></span>"
-        }
+  "notification static display ui": {
+    "connection": {
+      "can show friend request": {
+        "1": "<span id=\"tabSelector_connection\">friends<span id=\"LoadingIcon-react\"><b>·..</b></span></span>"
+      }
+    },
+    "my post": {
+      "can show request": {
+        "master": "<span id=\"tabSelector_outPost\">my posts<span class=\"text-danger\"> (1)</span></span>",
+        "request": "<span id=\"tabSelector_outPost_waitingList\">waiting list<span class=\"text-danger\"> (1)</span></span>"
       },
-      "my post": {
-        "can show request": {
-          "master": "<span id=\"tabSelector_outPost\">my posts<span class=\"text-danger\"> (1)</span></span>",
-          "request": "<span id=\"tabSelector_outPost_waitingList\">waiting list<span class=\"text-danger\"> (1)</span></span>"
-        },
-        "can show unaware return": {
-          "master": "<span id=\"tabSelector_outPost\">my posts<span class=\"text-danger\"> (1)</span></span>",
-          "return": "<span id=\"tabSelector_outPost_returnNote\">return<span class=\"text-danger\"> (1)</span></span>"
-        },
-        "can show combine request and unaware return": {
-          "master": "<span id=\"tabSelector_outPost\">my posts<span class=\"text-danger\"> (2)</span></span>",
-          "request": "<span id=\"tabSelector_outPost_waitingList\">waiting list<span class=\"text-danger\"> (1)</span></span>",
-          "return": "<span id=\"tabSelector_outPost_returnNote\">return<span class=\"text-danger\"> (1)</span></span>"
-        }
+      "can show unaware return": {
+        "master": "<span id=\"tabSelector_outPost\">my posts<span class=\"text-danger\"> (1)</span></span>",
+        "return": "<span id=\"tabSelector_outPost_returnNote\">return<span class=\"text-danger\"> (1)</span></span>"
       },
-      "friend post": {
-        "can show approve post": {
-          "master": "<span id=\"tabSelector_inPost\">friend posts<span class=\"text-danger\"> (1)</span></span>",
-          "approve": "<span id=\"tabSelector_inPost_approve\">approve<span class=\"text-danger\"> (1)</span></span>"
-        }
+      "can show combine request and unaware return": {
+        "master": "<span id=\"tabSelector_outPost\">my posts<span class=\"text-danger\"> (2)</span></span>",
+        "request": "<span id=\"tabSelector_outPost_waitingList\">waiting list<span class=\"text-danger\"> (1)</span></span>",
+        "return": "<span id=\"tabSelector_outPost_returnNote\">return<span class=\"text-danger\"> (1)</span></span>"
+      }
+    },
+    "friend post": {
+      "can show approve post": {
+        "master": "<span id=\"tabSelector_inPost\">friend posts<span class=\"text-danger\"> (1)</span></span>",
+        "approve": "<span id=\"tabSelector_inPost_approve\">approve<span class=\"text-danger\"> (1)</span></span>"
       }
     }
   }
