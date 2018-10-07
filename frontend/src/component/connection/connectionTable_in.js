d1e24cb01f7ff88e0541891c83cb86de1c60c238
Khanh Lu Tran <khanhlu2013@gmail.com>
2018-10-07 05:03:20 -0700

    Revert "Revert "dont know the case sensitive thing""
    This reverts commit 2cfb55f280051f27fdb57a07bdab9e7985e118ec.
    
    revert file insensitive fix.

---
+++ frontend/src/component/connection/connectionTable_in.js
@@ -0,0 +1,114 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { LoadingIcon } from "../../util/loadingIcon";
+
+const userNameColClass = "col-8";
+const decideColClass = "col-2 text-center";
+
+function Table(props) {
+  const {
+    connections,
+    loginUserId,
+    updateConnectionCb,
+    updatingConnectionIds
+  } = props;
+
+  const rows = connections.map(connection => (
+    <TableRow
+      key={connection.id}
+      connection={connection}
+      isUpdatingConnection={updatingConnectionIds.includes(connection.id)}
+      loginUserId={loginUserId}
+      updateConnectionCb={updateConnectionCb}
+    />
+  ));
+
+  return (
+    <div className="container-fluid">
+      <table
+        id="InFriendTable"
+        className="table table-sm table-striped table-bordered"
+      >
+        <thead className="thead-light">
+          <tr className="d-flex">
+            <th className={userNameColClass}>Friend requests</th>
+            <th className={decideColClass}>
+              <FontAwesomeIcon icon="thumbs-up" />
+            </th>
+            <th className={decideColClass}>
+              <FontAwesomeIcon icon="thumbs-down" />
+            </th>
+          </tr>
+        </thead>
+
+        <tbody>{rows}</tbody>
+      </table>
+    </div>
+  );
+}
+
+Table.propTypes = {
+  connections: PropTypes.array.isRequired,
+  updatingConnectionIds: PropTypes.array.isRequired,
+  loginUserId: PropTypes.string.isRequired,
+  updateConnectionCb: PropTypes.func.isRequired
+};
+
+function TableRow(props) {
+  const {
+    connection,
+    loginUserId,
+    updateConnectionCb,
+    isUpdatingConnection
+  } = props;
+
+  const approveClick = () => {
+    updateConnectionCb(connection.id, true);
+  };
+  const denyClick = () => {
+    updateConnectionCb(connection.id, false);
+  };
+
+  return (
+    <tr className="ConnectionTableRow d-flex">
+      <td className="col-8">
+        {connection.getTheOtherUser(loginUserId).getNameAndEmail()}
+      </td>
+
+      <td className={decideColClass}>
+        {isUpdatingConnection ? (
+          <LoadingIcon text={null} />
+        ) : (
+          <button
+            className="ConnectionTableRowApproveBtn btn btn-success"
+            onClick={approveClick}
+          >
+            <FontAwesomeIcon icon="thumbs-up" />
+          </button>
+        )}
+      </td>
+      <td className={decideColClass}>
+        {isUpdatingConnection ? (
+          <LoadingIcon text={null} />
+        ) : (
+          <button
+            className="ConnectionTableRowDenyBtn btn btn-warning"
+            onClick={denyClick}
+          >
+            <FontAwesomeIcon icon="thumbs-down" />
+          </button>
+        )}
+      </td>
+    </tr>
+  );
+}
+
+TableRow.propTypes = {
+  connection: PropTypes.object.isRequired,
+  isUpdatingConnection: PropTypes.bool.isRequired,
+  loginUserId: PropTypes.string.isRequired,
+  updateConnectionCb: PropTypes.func.isRequired
+};
+
+export { Table as ConnectionInTable };
---
+++ frontend/src/component/inPost/inPostItem_footing.js
@@ -0,0 +1,95 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { User } from "../../model/user";
+import { nullOrRequiredValidator } from "../../util";
+import { Share } from "../../model/share";
+import { InPostItemFootingApprove } from "./inPostItem_footing_approve";
+import { InPostItemFootingRequest } from "./inPostItem_footing_request";
+import { InPostItemFootingShop } from "./inPostItem_footing_shop";
+
+function InPostItemFooting(props) {
+  const {
+    postId,
+    loginUser,
+    isActive,
+    curBorrowShare,
+    myRequestShare,
+    isRequestingPost,
+    isDeleteingShare,
+    isAwaringShare,
+    isReturningShare,
+    onCreateShare,
+    onDeleteShare,
+    onAwareShare,
+    onReturnShare
+  } = props;
+
+  let content;
+
+  if (myRequestShare) {
+    content = (
+      <InPostItemFootingRequest
+        isDeleteingShare={isDeleteingShare}
+        onDeleteShare={onDeleteShare}
+        myRequestShareId={myRequestShare.id}
+      />
+    );
+  } else if (
+    curBorrowShare !== null &&
+    curBorrowShare.borrower.id === loginUser.id
+  ) {
+    content = (
+      <InPostItemFootingApprove
+        curBorrowShare={curBorrowShare}
+        isAwaringShare={isAwaringShare}
+        isReturningShare={isReturningShare}
+        onAwareShare={onAwareShare}
+        onReturnShare={onReturnShare}
+      />
+    );
+  } else if (isActive) {
+    content = (
+      <InPostItemFootingShop
+        postId={postId}
+        isRequestingPost={isRequestingPost}
+        onCreateShare={onCreateShare}
+      />
+    );
+  } else {
+    content = <div>Post is no longer active</div>;
+  }
+
+  return (
+    <div>
+      <div className="text-left">
+        {curBorrowShare &&
+          curBorrowShare.borrower.id !== loginUser.id && (
+            <span>
+              <span className="text-muted font-weight-light">
+                currently borrow by:
+              </span>
+              {curBorrowShare.borrower.getNameAndEmail()}
+            </span>
+          )}
+      </div>
+      <div className="text-right">{content}</div>
+    </div>
+  );
+}
+InPostItemFooting.propTypes = {
+  postId: PropTypes.string.isRequired,
+  loginUser: PropTypes.instanceOf(User).isRequired,
+  isActive: PropTypes.bool.isRequired,
+  curBorrowShare: nullOrRequiredValidator("object", Share),
+  myRequestShare: nullOrRequiredValidator("object", Share),
+  isRequestingPost: PropTypes.bool.isRequired,
+  isDeleteingShare: PropTypes.bool.isRequired,
+  isAwaringShare: PropTypes.bool.isRequired,
+  isReturningShare: PropTypes.bool.isRequired,
+  onCreateShare: PropTypes.func.isRequired,
+  onDeleteShare: PropTypes.func.isRequired,
+  onAwareShare: PropTypes.func.isRequired,
+  onReturnShare: PropTypes.func.isRequired
+};
+
+export { InPostItemFooting };
