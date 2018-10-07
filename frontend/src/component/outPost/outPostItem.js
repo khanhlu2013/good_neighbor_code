7ecdab1cb26945ff00a7040671f608a25cf9d9c3
Khanh Lu Tran <khanhlu2013@gmail.com>
2018-10-07 05:14:55 -0700

    Revert "Revert "Revert "Revert "dont know the case sensitive thing""""
    This reverts commit b0e3accdd877dd2a57c6c4568de245b9ce6a25af.
    
    :#

---
+++ frontend/src/component/outPost/outPostItem.js
@@ -0,0 +1,102 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { PostItemBody } from "../postItem_body";
+import { PostItemRequestList } from "../postItem_requestlist";
+import { OutPostItemHeading } from "./outPostItem_heading";
+import { LoadingIcon } from "../../util/loadingIcon";
+
+function OutPostItem(props) {
+  const {
+    post,
+    onEditPost,
+    onDecidePost,
+    isAwaringReturn,
+    onAwareReturnPost
+  } = props;
+
+  const onEditBtnClicked = e => {
+    onEditPost(post);
+  };
+
+  const onDecidePostClick = e => {
+    onDecidePost(post);
+  };
+
+  const onAwareReturnClick = e => {
+    onAwareReturnPost(post.id);
+  };
+
+  const curBorrowShare = post.curBorrowShare;
+  const borrower = curBorrowShare ? curBorrowShare.borrower : null;
+
+  return (
+    <div id="outPost-item-react" className="post-item">
+      <OutPostItemHeading dateCreate={post.dateCreate} />
+
+      <div className="container">
+        <PostItemBody title={post.title} description={post.description} />
+        {post.requestShares.length !== 0 && (
+          <PostItemRequestList shares={post.requestShares} />
+        )}
+
+        <div className="text-left">
+          {borrower && (
+            <span>
+              <span className="text-muted font-weight-light">
+                currently borrow by:{" "}
+              </span>
+              {borrower.getNameAndEmail()}
+            </span>
+          )}
+        </div>
+        <div className="text-right">
+          {post.unawareReturnShareLatest && (
+            <span>
+              {`item is returned by ${post.unawareReturnShareLatest.borrower.getNameAndEmail()}`}
+              {isAwaringReturn ? (
+                <LoadingIcon text={"receiving"} />
+              ) : (
+                <button
+                  id="outPostItem-awareReturnBtn-react"
+                  onClick={onAwareReturnClick}
+                  className="btn btn-success"
+                >
+                  confirm returned
+                </button>
+              )}
+            </span>
+          )}
+          <button
+            id="outPostItem-editBtn-react"
+            onClick={onEditBtnClicked}
+            className="btn btn-primary"
+          >
+            edit
+          </button>
+          {(post.denyShares.length !== 0 ||
+            post.requestShares.length !== 0 ||
+            curBorrowShare) && (
+            <button
+              id="outPostItem-decisionBtn-react"
+              onClick={onDecidePostClick}
+              className="btn btn-success"
+            >
+              share
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+OutPostItem.propTypes = {
+  post: PropTypes.object.isRequired,
+  onEditPost: PropTypes.func.isRequired,
+  onDecidePost: PropTypes.func.isRequired,
+  onAwareReturnPost: PropTypes.func.isRequired,
+  isAwaringReturn: PropTypes.bool.isRequired
+};
+
+export { OutPostItem };
---
+++ frontend/src/component/outPost/outPostManagement.js
@@ -0,0 +1,357 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Tabs, Tab, TabList, TabPanel } from "react-tabs";
+
+import { OutPostCrudDialog } from "./crudDialog";
+import { API } from "../../api/profile-api";
+import { OutPostDecisionDialog } from "./decisionDialog";
+import { Post } from "../../model/post";
+import { OutShareHistoryList } from "./outShareHistoryList";
+import { OutPostList } from "./outPostList";
+import { NotificationItem } from "../../util/notificationItem";
+import { LoadingIcon } from "../../util/loadingIcon";
+
+class OutPostManagement extends Component {
+  state = {
+    posts: null,
+    awaringReturnPostIds: [],
+
+    //crud
+    curCrudPostSessionID: null,
+    isOpenCrudDialog: false,
+    curEditPost: null,
+    isCrudingPost: false,
+
+    //decision
+    isOpenDecisionDialog: false,
+    curDecidePost: null,
+    isDecidingPost: false
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    let returnShares = null;
+    let requestNotePosts = null;
+    let borrowPosts = null;
+    let returnNotePosts = null;
+
+    const { posts } = state;
+
+    if (posts) {
+      const returnShares2D = posts.map(post =>
+        post.shares.filter(share => share.isReturn)
+      );
+      returnShares = [].concat(...returnShares2D);
+
+      requestNotePosts = posts.filter(post => post.isNote_requestWithNoBorrow);
+      borrowPosts = posts.filter(post => post.curBorrowShare);
+      returnNotePosts = posts.filter(post => post.unawareReturnShareLatest);
+    }
+    return {
+      returnShares,
+      requestNotePosts,
+      borrowPosts,
+      returnNotePosts
+    };
+  }
+
+  componentDidMount() {
+    this.props.onOutPostNotify(null);
+    (async () => {
+      this.setPostsState(await API.outPosts());
+    })();
+  }
+
+  setPostsState(posts) {
+    this.setState({ posts });
+    const requestPostCount = posts.filter(
+      post => post.isNote_requestWithNoBorrow
+    ).length;
+
+    this.props.onOutPostNotify(
+      requestPostCount + this.state.returnNotePosts.length
+    );
+  }
+
+  onAwareReturnPost = postId => {
+    this.props.onOutPostNotify(null);
+    this.setState({
+      awaringReturnPostIds: [...this.state.awaringReturnPostIds, postId]
+    });
+
+    (async () => {
+      await API.awareReturnPost(postId);
+      const curPost = this.state.posts.find(post => post.id === postId);
+      for (const share of curPost.returnShares) {
+        share.isAwareReturn = true;
+      }
+
+      this.setPostsState([
+        ...this.state.posts.filter(post => post.id !== curPost.id),
+        curPost
+      ]);
+      this.setState({
+        awaringReturnPostIds: this.state.awaringReturnPostIds.filter(
+          id => id !== postId
+        )
+      });
+    })();
+  };
+
+  // CRUD START --------------------------------
+  onOpenCrudDialog_create = () => {
+    const post = null;
+    this.onOpenCrudDialog_edit(post);
+  };
+
+  onOpenCrudDialog_edit = post => {
+    this.setState({
+      isOpenCrudDialog: true,
+      curEditPost: post,
+      curCrudPostSessionID: Date.now().toString()
+    });
+  };
+
+  onCrudDialogOk = (postID, title, description, isActive) => {
+    this.setState({
+      isCrudingPost: true
+    });
+
+    (async () => {
+      if (postID) {
+        const {
+          updatedTitle,
+          updatedDescription,
+          updatedIsActive
+        } = await API.updatePost(postID, title, description, isActive);
+        const [oldPost] = this.state.posts.filter(post => post.id === postID);
+        oldPost.title = updatedTitle;
+        oldPost.description = updatedDescription;
+        oldPost.isActive = updatedIsActive;
+        this.setState({
+          posts: [
+            ...this.state.posts.filter(post => post.id !== postID),
+            oldPost
+          ]
+        });
+      } else {
+        const {
+          createdId,
+          createdIsActive,
+          createdTitle,
+          createdDescription,
+          createdDateCreated
+        } = await API.createPost(title, description, isActive);
+        const newPost = new Post(
+          createdId,
+          this.props.loginUser,
+          createdIsActive,
+          createdTitle,
+          createdDescription,
+          new Date(createdDateCreated),
+          []
+        );
+        this.setState({ posts: [...this.state.posts, newPost] });
+      }
+      this.setState({ isCrudingPost: false, isOpenCrudDialog: false });
+    })();
+  };
+
+  onCrudDialogCancel = () => {
+    this.setState({ isOpenCrudDialog: false });
+  };
+
+  // CRUD END --------------------------------
+
+  // DECISION START --------------------------
+  onOpenDecideDialog = post => {
+    this.setState({
+      isOpenDecisionDialog: true,
+      curDecidePost: post
+    });
+  };
+
+  onExitDecisionDialog = () => {
+    this.setState({
+      isOpenDecisionDialog: false,
+      curDecidePost: null
+    });
+  };
+
+  _approveShare = (shareID, isApprove) => {
+    this.setState({ isDecidingPost: true });
+    (async () => {
+      const decidedIsApprove = await API.approveShare(shareID, isApprove);
+      const [decidedPost] = this.state.posts.filter(post =>
+        post.shares.some(share => share.id === shareID)
+      );
+      const [decidedShare] = decidedPost.shares.filter(
+        share => share.id === shareID
+      );
+      decidedShare.isApprove = decidedIsApprove;
+      decidedPost.shares = [
+        ...decidedPost.shares.filter(share => share.id !== shareID),
+        decidedShare
+      ];
+      this.setPostsState([
+        ...this.state.posts.filter(post => post.id !== decidedPost.id),
+        decidedPost
+      ]);
+      this.setState({ curDecidePost: decidedPost, isDecidingPost: false });
+    })();
+  };
+
+  onUndoDeniedShare = shareID => {
+    this._approveShare(shareID, undefined);
+  };
+
+  onUndoApproveShare = shareID => {
+    this._approveShare(shareID, undefined);
+  };
+
+  onDecideShare = (shareID, isApprove) => {
+    if (isApprove === undefined) {
+      throw Error("Unexpected decision");
+    }
+
+    this._approveShare(shareID, isApprove);
+  };
+  // DECISION END --------------------------
+
+  _genPostList = (listId, posts) => (
+    <OutPostList
+      listId={listId}
+      posts={posts}
+      onEditPost={this.onOpenCrudDialog_edit}
+      onDecidePost={this.onOpenDecideDialog}
+      onAwareReturnPost={this.onAwareReturnPost}
+      awaringReturnPostIds={this.state.awaringReturnPostIds}
+    />
+  );
+
+  _getPostsContent() {
+    const {
+      posts,
+      requestNotePosts,
+      borrowPosts,
+      returnNotePosts
+    } = this.state;
+
+    return (
+      <Tabs forceRenderTabPanel={true}>
+        <TabList>
+          <button
+            id="createPostBtn"
+            onClick={this.onOpenCrudDialog_create}
+            className="btn btn-sm btn-success mr-4"
+          >
+            new post
+          </button>
+          <Tab>
+            <span id="tabSelector_outPost_all">
+              all
+              <NotificationItem count={posts.length} isImportant={false} />
+            </span>
+          </Tab>
+          <Tab>
+            <span id="tabSelector_outPost_waitingList">
+              request
+              <NotificationItem
+                count={requestNotePosts.length}
+                isImportant={true}
+              />
+            </span>
+          </Tab>
+          <Tab>
+            <span id="tabSelector_outPost_borrow">
+              borrow
+              <NotificationItem
+                count={borrowPosts.length}
+                isImportant={false}
+              />
+            </span>
+          </Tab>
+          <Tab>
+            <span id="tabSelector_outPost_returnNote">
+              return
+              <NotificationItem
+                count={returnNotePosts.length}
+                isImportant={false}
+              />
+            </span>
+          </Tab>
+          <Tab>
+            <span id="tabSelector_outPost_history">
+              history
+              <NotificationItem
+                count={this.state.returnShares.length}
+                isImportant={false}
+              />
+            </span>
+          </Tab>
+        </TabList>
+
+        <TabPanel>{this._genPostList("outPostList-all-react", posts)}</TabPanel>
+        <TabPanel>
+          {this._genPostList("outPostList-requestNote-react", requestNotePosts)}
+        </TabPanel>
+        <TabPanel>
+          {this._genPostList("outPostList-borrow-react", borrowPosts)}
+        </TabPanel>
+        <TabPanel>
+          {this._genPostList("outPostList-returnNote-react", returnNotePosts)}
+        </TabPanel>
+        <TabPanel>
+          <OutShareHistoryList shares={this.state.returnShares} />
+        </TabPanel>
+      </Tabs>
+    );
+  }
+
+  render() {
+    let content;
+    if (this.state.posts) {
+      content = this._getPostsContent();
+    } else {
+      content = (
+        <h1 className="text-center">
+          <LoadingIcon text="loading" />
+        </h1>
+      );
+    }
+
+    return (
+      <div id="outPostManagement-react">
+        {content}
+
+        {this.state.curCrudPostSessionID && (
+          <OutPostCrudDialog
+            key={this.state.curCrudPostSessionID}
+            isOpen={this.state.isOpenCrudDialog}
+            post={this.state.curEditPost}
+            isCrudingPost={this.state.isCrudingPost}
+            onOk={this.onCrudDialogOk}
+            onCancel={this.onCrudDialogCancel}
+          />
+        )}
+
+        {this.state.isOpenDecisionDialog && (
+          <OutPostDecisionDialog
+            isOpen={this.state.isOpenDecisionDialog}
+            post={this.state.curDecidePost}
+            isDecidingPost={this.state.isDecidingPost}
+            onUndoApproveShare={this.onUndoApproveShare}
+            onUndoDeniedShare={this.onUndoDeniedShare}
+            onDecideShare={this.onDecideShare}
+            onExit={this.onExitDecisionDialog}
+          />
+        )}
+      </div>
+    );
+  }
+}
+OutPostManagement.propType = {
+  loginUser: PropTypes.object.isRequired,
+  onOutPostNotify: PropTypes.func.isRequired
+};
+
+export { OutPostManagement };
