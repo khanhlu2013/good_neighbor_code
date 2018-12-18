24afb3b33f9da0b12ea50586e48882aa4673f3e4
Khanh Lu Tran <khanhlu2013@gmail.com>
2018-12-18 12:30:24 -0800

    provide icon for bottom tab

--- mobile/src/bus/inPost/view/inPostManagement.mobileView.js
+++ mobile/src/bus/inPost/view/inPostManagement.mobileView.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { Text, View, Button } from "react";
 import styled from "styled-components";
 import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import { Icon } from "native-base";
+
 import InPostListMobileView from "./inPostList.mobileView";
 import InPostManagementPropType from "../../../common/bus/inPost/propType/inPostManagement.propType";
 import LoadingIconMobileView from "../../../share/LoadingIcon.mobileView";
@@ -22,36 +25,96 @@ function InPostManagementMobileView(props) {
     approveAlertPosts,
     returnShares
   } = props;
-  const nav = createBottomTabNavigator({
-    all: {
-      screen: props => (
-        <InPostListMobileView listId="blahblahID" posts={posts} />
-      )
+  const nav = createBottomTabNavigator(
+    {
+      all: {
+        screen: props => (
+          <InPostListMobileView listId="blahblahID" posts={posts} />
+        ),
+        navigationOptions: {
+          title: null,
+          tabBarLabel: `all (${posts.length})`
+        }
+      },
+      request: {
+        screen: props => (
+          <InPostListMobileView listId="blahblahID" posts={requestPosts} />
+        )
+      },
+      approve: {
+        screen: props => (
+          <InPostListMobileView listId="blahblahID" posts={approveAlertPosts} />
+        )
+      },
+      borrow: {
+        screen: props => (
+          <InPostListMobileView listId="blahblahID" posts={borrowPosts} />
+        )
+      },
+      history: {
+        screen: props => (
+          <InPostUserHistoryListController
+            shares={returnShares}
+            view={InPostUserHistoryListMobileView}
+          />
+        )
+      }
     },
-    request: {
-      screen: props => (
-        <InPostListMobileView listId="blahblahID" posts={requestPosts} />
-      )
-    },
-    approve: {
-      screen: props => (
-        <InPostListMobileView listId="blahblahID" posts={approveAlertPosts} />
-      )
-    },
-    borrow: {
-      screen: props => (
-        <InPostListMobileView listId="blahblahID" posts={borrowPosts} />
-      )
-    },
-    history: {
-      screen: props => (
-        <InPostUserHistoryListController
-          shares={returnShares}
-          view={InPostUserHistoryListMobileView}
-        />
-      )
+    {
+      defaultNavigationOptions: ({ navigation }) => ({
+        tabBarIcon: ({ focused, horizontal, tintColor }) => {
+          const { routeName } = navigation.state;
+          let iconName;
+          let iconProvider;
+          let caption;
+          switch (routeName) {
+            case "all":
+              iconName = "ios-globe";
+              iconProvider = "Ionicons";
+              caption = "all friend posts";
+              break;
+            case "request":
+              iconName = "question";
+              iconProvider = "FontAwesome";
+              caption = "request";
+              break;
+            case "approve":
+              iconName = "check";
+              iconProvider = "FontAwesome";
+              caption = "approve alert";
+              break;
+            case "borrow":
+              iconName = "slideshare";
+              iconProvider = "Entypo";
+              caption = "borrow";
+              break;
+            case "history":
+              iconName = "history";
+              iconProvider = "FontAwesome";
+              caption = "history";
+              break;
+            default:
+              iconName = "cancel";
+              iconProvider = "MaterialCommunityIcons";
+              caption = null;
+              break;
+          }
+          return (
+            <Icon
+              name={iconName}
+              type={iconProvider}
+              style={{ fontSize: 27, color: tintColor }}
+            />
+          );
+        }
+      }),
+      tabBarOptions: {
+        activeTintColor: "tomato",
+        inactiveTintColor: "gray",
+        showLabel: true
+      }
     }
-  });
+  );
 
   const container = createAppContainer(nav);
   let content;
