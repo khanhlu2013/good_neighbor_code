3dd123dbe8d6266c77f02ae3e17a6daee8aefb34
lu <lu@pop-os.localdomain>
2023-03-30 14:15:30 -0700

    Updates

---
+++ precache-manifest.b813ba64bee17f07d1d8228d13815d1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e4d083bd1161924e684",
+    "url": "/good_neighbor_test/static/css/main.7a4b86f7.chunk.css"
+  },
+  {
+    "revision": "8e4d083bd1161924e684",
+    "url": "/good_neighbor_test/static/js/main.8e4d083b.chunk.js"
+  },
+  {
+    "revision": "a825a08f542513814d9d",
+    "url": "/good_neighbor_test/static/css/1.cef56732.chunk.css"
+  },
+  {
+    "revision": "a825a08f542513814d9d",
+    "url": "/good_neighbor_test/static/js/1.a825a08f.chunk.js"
+  },
+  {
+    "revision": "522ed172bc502e09142f",
+    "url": "/good_neighbor_test/static/js/runtime~main.522ed172.js"
+  },
+  {
+    "revision": "f8108a387b21f1a48e0d160b35e2c271",
+    "url": "/good_neighbor_test/index.html"
+  }
+];
